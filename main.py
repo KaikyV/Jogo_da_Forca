47a5362a94868e08a73e5c10fc63355c8bf1e89a
Kaiky Verissimo <167787476+KaikyV@users.noreply.github.com>
2024-07-05 17:34:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-# Auto detect text files and perform LF normalization
-* text=auto
-#apenas testando
-#teste 2
-#testeb3
+#import 
+
+def main():
+    while True:
+        opcao = int(input("""O'que você deseja?
+          Insira 1 para inciar um novo jogo;
+          Insira 2 para verificar record gerais;
+          Insira 3 para verificar record de um determinado jogador;
+          Insira 4 para encerrar o programa.
+                          """))
+        if opcao == 1:
+            print('a')
+        elif opcao == 2:
+            print('a')
+        elif opcao == 3:
+            print('a')
+        elif opcao == 4:
+            print('Obrigado por jogar!')
+            break
+        else:
+            print('Opção inválida. Tente novamente.')
+main()
