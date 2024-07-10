faa988ec01437fd506df31fc0b955c4deeb2e58d
Kaiky Verissimo <167787476+KaikyV@users.noreply.github.com>
2024-07-10 15:44:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-#import 
-
 def main():
+    cabeçario()
+    pedir_nick()
+    #placar()
     while True:
-        opcao = int(input("""O'que você deseja?
+        opcao = int(input("""O'que você deseja?: """+str(nick)+""""
           Insira 1 para inciar um novo jogo;
           Insira 2 para verificar record gerais;
           Insira 3 para verificar record de um determinado jogador;
           Insira 4 para encerrar o programa.
                           """))
         if opcao == 1:
-            print('a')
+            dificuldade()
+            placar()
         elif opcao == 2:
             print('a')
         elif opcao == 3:
             print('a')
         elif opcao == 4:
             print('Obrigado por jogar!')
+            #salvar_record(lista_records)
             break
         else:
             print('Opção inválida. Tente novamente.')
-main()
+
+if __name__ == "__main__":
+    main()
