85c023bba0757658d4d0e8f681eca087542ddc4e
Kaiky Verissimo <167787476+KaikyV@users.noreply.github.com>
2024-07-18 13:24:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
+from Interface import *
+from dados import *
+from verificação import *
+
 def main():
     cabeçario()
-    pedir_nick()
-    #placar()
     while True:
-        opcao = int(input("""O'que você deseja?: """+str(nick)+""""
-          Insira 1 para inciar um novo jogo;
-          Insira 2 para verificar record gerais;
-          Insira 3 para verificar record de um determinado jogador;
-          Insira 4 para encerrar o programa.
-                          """))
-        if opcao == 1:
-            dificuldade()
-            placar()
-        elif opcao == 2:
-            print('a')
-        elif opcao == 3:
-            print('a')
-        elif opcao == 4:
-            print('Obrigado por jogar!')
-            #salvar_record(lista_records)
-            break
-        else:
-            print('Opção inválida. Tente novamente.')
+            opcao = int(input("""O'que você deseja?
+            Insira 1 para inciar um novo jogo;
+            Insira 2 para verificar record gerais;
+            Insira 3 para verificar record de um determinado jogador;
+            Insira 4 para encerrar o programa.
+                             """))
+            if opcao == 1:
+                pedir_nick()
+                dificuldade()
+                jogo_da_forca()
+            elif opcao == 2:
+                mostrar_records()
+            elif opcao == 3:
+                buscar_dados_jogador()
+            elif opcao == 4:
+                print('Obrigado por jogar!')
+                #salvar_record(lista_records)
+                break
+            else:
+                print('Opção inválida. Tente novamente.')
 
+# Executar o programa
 if __name__ == "__main__":
     main()
